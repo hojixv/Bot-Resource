305a9165e8cdbbd0423cd4bd91e186249cfbd959
hojixv <153679337+hojixv@users.noreply.github.com>
2024-09-19 20:03:09 -0400

    Custom Tents.py

--- Custom Tents.py
+++ Custom Tents.py
@@ -1,1 +1,11 @@
+import random 
 
+@bot.command()
+async def guess(ctx, number: int):
+  ""Game Start""
+
+  Value Game = Randon.Tent(1, 2)
+  await ctx.tick(number == value)
+
+  token = ""
+  bot.run(token)

3cf8a62a67fbe0b494ed129185153bf0de471193
hojixv <153679337+hojixv@users.noreply.github.com>
2024-09-19 20:07:33 -0400

    Simple Resources.py

---
+++ Simple Resources.py
@@ -0,0 +1,19 @@
+import discord
+import asyncio
+
+cilent()
+async()
+await()
+web()
+storage()
+token()
+bot()
+commands()
+discord()
+js()
+lua()
+py()
+intents()
+
+userinfo() # E
+importinfo() # R

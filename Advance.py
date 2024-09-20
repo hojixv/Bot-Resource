740dc3025c4afeb33be66c0ce16bcb244fb02635
hojixv <153679337+hojixv@users.noreply.github.com>
2024-09-19 20:05:34 -0400

    Advance.py

--- Advance.py
+++ Advance.py
@@ -1,1 +1,19 @@
+import asyncio
+import logging
+import logging.handlers
+import os import asyncpg  # From Ivi 
+import discord
+from discord.ext import commands
+from aiohttp import ClientSession
 
+ for extension in self.initial_extensions:
+    await self.load_extension(extension)
+      if self hosting await()
+
+async def guess
+
+@bot.command()
+guess(1, 4)
+value = random.randint(1, 4, 5)
+
+asyncio.run(main())
